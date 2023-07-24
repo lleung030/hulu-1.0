86a7eabaa26e57ebaafa66b75840d7492a309b78
lleung030 <lucasleung2593@gmail.com>
2023-07-24 10:39:57 -0700

    image poster display map

--- app/page.js
+++ app/page.js
@@ -2,19 +2,36 @@ import Head from 'next/head';
 import styles from '@/styles/global.css';
 import Header from '@/components/Header';
 import NavBar from '@/components/NavBar';
+import Results from '@/components/Results';
+import requests from '@/utils/requests';
 
-export default function Home() {
+
+export default function Home({results}) {
+
     return (
         <div className={styles.container}>
             <Head>
+                <title>Hulu App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-        <h1>
-            Hulu App
-        </h1>
+        
         <Header />
         <NavBar />
+
+        <Results results={results}/>
         </div>
     )
 }
+
+// export async function getServerSideProps(context) {
+//     const genre = context.query.genre
+
+//     const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
+//     ).then((res) => res.json());
+//     return {
+//         props: {
+//             results: request.results
+//         }
+//     }
+// }
--- components/Results.jsx
+++ components/Results.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+"use client";
 
-function Results() {
+import Thumbnail from "./Thumbnail";
+import React, { useState, useEffect } from "react";
+import requests from "@/utils/requests";
+
+function Results({ results }) {
+  const [data, setData] = useState([]);
+  const [fetchedData, setFetchedData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // const genre = context.query.genre;
+      const genre = "Action";
+      const request = await fetch(
+        `https://api.themoviedb.org/3${
+          requests[genre]?.url || requests.fetchTrending.url
+        }`
+      ).then((res) => res.json());
+      const results = request.results;
+      setFetchedData(results);
+    };
+
+    fetchData();
+  }, []);
   return (
     <div>
-      
+      {/* {results.map(result => (
+        <Thumbnail key={result.id} result={result}/>
+      ))} */}
+      {fetchedData.map((result) => (
+        <Thumbnail key={result.id} result={result}/>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Results
+export default Results;
